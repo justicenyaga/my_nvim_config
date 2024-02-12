88f62495fc34f6a6c850e164eab36944138bd635
Justice Nyaga <justiceevansmwema@gmail.com>
2024-02-12 21:35:23 +0300

    Uncomment the win32yank block

--- lua/justice/core/options.lua
+++ lua/justice/core/options.lua
@@ -49,18 +49,18 @@ opt.clipboard = "unnamedplus" -- use system clipboard as default register
 
 -- IF ON WSL, UNCOMMENT THIS BLOCK TO INTERACT WITH WINDOWS CLIPBOARD
 
--- vim.g.clipboard = {
---   name = "win32yank-wsl",
---   copy = {
---     ["+"] = "win32yank.exe -i --crlf",
---     ["*"] = "win32yank.exe -i --crlf",
---   },
---   paste = {
---     ["+"] = "win32yank.exe -o --lf",
---     ["*"] = "win32yank.exe -o --lf",
---   },
---   cache_enabled = true,
--- }
+vim.g.clipboard = {
+  name = "win32yank-wsl",
+  copy = {
+    ["+"] = "win32yank.exe -i --crlf",
+    ["*"] = "win32yank.exe -i --crlf",
+  },
+  paste = {
+    ["+"] = "win32yank.exe -o --lf",
+    ["*"] = "win32yank.exe -o --lf",
+  },
+  cache_enabled = true,
+}
 
 -- split windows
 opt.splitright = true -- split vertical window to the right
