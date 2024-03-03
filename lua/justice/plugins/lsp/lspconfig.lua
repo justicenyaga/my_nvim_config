5d420fb38f6fe2528fbdbaeeeaf33ac0b2739bc3
Justice Nyaga <justiceevansmwema@gmail.com>
2024-03-03 13:04:35 +0300

    Remove tsserver code 80001 diagnostic message

--- lua/justice/plugins/lsp/lspconfig.lua
+++ lua/justice/plugins/lsp/lspconfig.lua
@@ -93,6 +93,21 @@ return {
     lspconfig["tsserver"].setup({
       capabilities = capabilities,
       on_attach = on_attach,
+      handlers = {
+        ["textDocument/publishDiagnostics"] = function(_, result, ctx, config)
+          if result.diagnostics ~= nil then
+            local idx = 1
+            while idx <= #result.diagnostics do
+              if result.diagnostics[idx].code == 80001 then
+                table.remove(result.diagnostics, idx)
+              else
+                idx = idx + 1
+              end
+            end
+          end
+          vim.lsp.diagnostic.on_publish_diagnostics(_, result, ctx, config)
+        end,
+      },
     })
 
     -- configure css server
