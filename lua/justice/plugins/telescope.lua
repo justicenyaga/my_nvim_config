c486174ad83a6617552e684df53fcc2eaad0ceff
Justice Nyaga <justiceevansmwema@gmail.com>
2024-03-08 22:04:16 +0300

    Add todo-comments plugin

--- lua/justice/plugins/telescope.lua
+++ lua/justice/plugins/telescope.lua
@@ -34,5 +34,6 @@ return {
     keymap.set("n", "<leader>fs", "<cmd>Telescope live_grep<cr>", { desc = "Find string in cwd" })
     keymap.set("n", "<leader>fc", "<cmd>Telescope grep_string<cr>", { desc = "Find string under cursor in cwd" })
     keymap.set("n", "<leader>fh", "<cmd>Telescope harpoon marks<cr>", { desc = "Fuzzy find harpoon marked files" })
+    keymap.set("n", "<leader>ft", "<cmd>TodoTelescope<CR>", { desc = "Fuzzy find todos" })
   end,
 }
