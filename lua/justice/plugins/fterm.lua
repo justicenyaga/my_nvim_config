bc8ef52ccc19f57576bab3001eb69cb74be22e7f
Justice Nyaga <justiceevansmwema@gmail.com>
2024-03-13 12:20:07 +0300

    Use default fterm window

--- lua/justice/plugins/fterm.lua
+++ lua/justice/plugins/fterm.lua
@@ -16,10 +16,11 @@ return {
       -- Toggle the FTerm terminal in normal mode
       fTerm.toggle()
       -- Create a namespace for the current buffer's filetype
-      local ns = api.nvim_create_namespace(vim.bo.filetype)
+      -- local ns = api.nvim_create_namespace(vim.bo.filetype)
       -- Set the highlight namespace for the current window
-      api.nvim_win_set_hl_ns(0, ns)
-      api.nvim_set_hl(ns, "Normal", { bg = "#15151c" })
+      -- api.nvim_win_set_hl_ns(0, ns)
+      -- api.nvim_set_hl(ns, "Normal", { bg = "#15151c" })
+      -- api.nvim_set_hl(ns, "Normal")
     end, {})
     keymap.set("t", "<C-z>", function()
       -- Toggle the FTerm terminal in terminal mode
@@ -29,27 +30,27 @@ return {
         false
       )
     end)
-    -- Configure FTerm settings
-    require("FTerm").setup({
-      border = "none",
-      ft = "terminal",
-      dimensions = {
-        height = 0.5,
-        width = 1,
-        x = 0, -- X axis of the terminal window
-        y = 1, -- Y axis of the terminal window
-      },
-    })
-    api.nvim_create_autocmd("FileType", {
-      pattern = "terminal",
-      callback = function()
-        -- Create a namespace for the current buffer's filetype
-        local ns = api.nvim_create_namespace(vim.bo.filetype)
-        -- Set the highlight namespace for the current window
-        api.nvim_win_set_hl_ns(0, ns)
-        -- Set the background color for the highlight namespace
-        api.nvim_set_hl(ns, "Normal", { bg = "#171717" })
-      end,
-    })
+    -- -- Configure FTerm settings
+    -- require("FTerm").setup({
+    --   border = "none",
+    --   ft = "terminal",
+    --   dimensions = {
+    --     height = 0.5,
+    --     width = 1,
+    --     x = 0, -- X axis of the terminal window
+    --     y = 1, -- Y axis of the terminal window
+    --   },
+    -- })
+    -- api.nvim_create_autocmd("FileType", {
+    --   pattern = "terminal",
+    --   callback = function()
+    --     -- Create a namespace for the current buffer's filetype
+    --     local ns = api.nvim_create_namespace(vim.bo.filetype)
+    --     -- Set the highlight namespace for the current window
+    --     api.nvim_win_set_hl_ns(0, ns)
+    --     -- Set the background color for the highlight namespace
+    --     api.nvim_set_hl(ns, "Normal", { bg = "#171717" })
+    --   end,
+    -- })
   end,
 }
