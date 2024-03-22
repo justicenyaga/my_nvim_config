ae22ff525872897aa89f7995cb27b3a86f6d1e3a
Justice Nyaga <justiceevansmwema@gmail.com>
2024-03-22 19:10:30 +0300

    Add support for formatting ejs files

--- lua/justice/plugins/formatting.lua
+++ lua/justice/plugins/formatting.lua
@@ -22,6 +22,22 @@ return {
         lua = { "stylua" },
         python = { "isort", "black" },
       },
+      formatters = {
+        prettier = {
+          args = function(self, ctx)
+            if vim.endswith(ctx.filename, ".ejs") then
+              return { "--stdin-filepath", "$FILENAME", "--parser", "html" }
+            end
+            return { "--stdin-filepath", "$FILENAME" }
+          end,
+          range_args = function(self, ctx, range)
+            if vim.endswith(ctx.filename, ".ejs") then
+              return { "--stdin-filepath", "$FILENAME", "--parser", "html", "--range", range }
+            end
+            return { "--stdin-filepath", "$FILENAME", "--range", range }
+          end,
+        },
+      },
       format_on_save = {
         lsp_fallback = true,
         async = false,
