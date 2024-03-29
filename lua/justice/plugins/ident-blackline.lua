10cbb7c652d274fcd4f3e5426d2accf369e0d3fc
Justice Nyaga <justiceevansmwema@gmail.com>
2024-03-29 18:42:06 +0300

    Change ibl char to a thinner vertical line

--- lua/justice/plugins/ident-blackline.lua
+++ lua/justice/plugins/ident-blackline.lua
@@ -32,7 +32,10 @@ return {
     end)
 
     vim.g.rainbow_delimiters = { highlight = highlight }
-    require("ibl").setup({ scope = { highlight = highlight } })
+    require("ibl").setup({
+      indent = { char = "▏" },
+      scope = { highlight = highlight },
+    })
 
     hooks.register(hooks.type.SCOPE_HIGHLIGHT, hooks.builtin.scope_highlight_from_extmark)
   end,
