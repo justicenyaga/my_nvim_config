608d68ef6040b319ebf64521bbe2e57b77b39893
Justice Nyaga <justiceevansmwema@gmail.com>
2024-03-29 18:09:39 +0300

    Change order of ibl highlight colors

--- lua/justice/plugins/ident-blackline.lua
+++ lua/justice/plugins/ident-blackline.lua
@@ -9,23 +9,23 @@ return {
     local set_hl = vim.api.nvim_set_hl -- for conciseness
 
     local highlight = {
-      "RainbowRed",
       "RainbowYellow",
-      "RainbowBlue",
-      "RainbowOrange",
-      "RainbowGreen",
       "RainbowViolet",
+      "RainbowBlue",
+      "RainbowGreen",
       "RainbowCyan",
+      -- "RainbowRed",
+      -- "RainbowOrange",
     }
 
     local hooks = require("ibl.hooks")
     -- create the highlight groups in the highlight setup hook, so they are reset
     -- everytime the colorscheme changes
     hooks.register(hooks.type.HIGHLIGHT_SETUP, function()
-      set_hl(0, "RainbowRed", { fg = "#E06C75" })
+      -- set_hl(0, "RainbowRed", { fg = "#E06C75" })
       set_hl(0, "RainbowYellow", { fg = "#E5C07B" })
       set_hl(0, "RainbowBlue", { fg = "#61AFEF" })
-      set_hl(0, "RainbowOrange", { fg = "#D19A66" })
+      -- set_hl(0, "RainbowOrange", { fg = "#D19A66" })
       set_hl(0, "RainbowGreen", { fg = "#98C379" })
       set_hl(0, "RainbowViolet", { fg = "#C678DD" })
       set_hl(0, "RainbowCyan", { fg = "#56B6C2" })
