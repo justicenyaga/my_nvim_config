135a943f8f732235bd26e69d4499c24557a8f679
Justice Nyaga <justiceevansmwema@gmail.com>
2024-04-15 19:47:53 +0300

    Fix typo on trouble keymap desc

--- lua/justice/plugins/trouble.lua
+++ lua/justice/plugins/trouble.lua
@@ -6,7 +6,7 @@ return {
     { "<leader>xw", ":TroubleToggle workspace_diagnostics<CR>", desc = "Open trouble workspace diagnostics" },
     { "<leader>xd", ":TroubleToggle document_diagnostics<CR>", desc = "Open trouble document diagnostics" },
     { "<leader>xq", ":TroubleToggle quickfix<CR>", desc = "Open trouble quickfix list" },
-    { "<leader>xl", ":TroubleToggle loclist<CR>", desc = "Open trouble location fist" },
+    { "<leader>xl", ":TroubleToggle loclist<CR>", desc = "Open trouble location list" },
     { "<leader>xt", ":TodoTrouble<CR>", desc = "Open todos in trouble" },
   },
 }
